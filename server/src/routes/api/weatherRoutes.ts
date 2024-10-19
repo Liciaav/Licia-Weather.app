d32021c03059cdf6aa19a02edf9f38a757b94776
Liciaav <liciaavasc@gmail.com>
2024-10-18 20:41:08 -0400

    update weatherroutes.ts and htmlRoutes.ts.

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,13 +1,25 @@
 import { Router, type Request, type Response } from 'express';
-const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from './server/src/service/historyService.ts';
+import WeatherService from './server/src/service/weatherService.ts';
+
+const router = Router();
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  const{cityName} = req.body;
+  if (!cityName) {
+    return res.status(400).json({erro: 'City name is required.'});
+  }
+  try{
+    const weatherData = await WeatherService. get WheatherByCity(cityName);
   // TODO: save city to search history
+  await HistoryService.saveCityToHistory(cityName);
+  return res.status(200).json(weatherData);
+  } catch (erro){
+    return res.status(500).json({erro: 'Failed to retrive wearther data.'});
+  }
 });
 
 // TODO: GET search history
